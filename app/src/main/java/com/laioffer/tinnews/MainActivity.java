370e1fd5b252eafac236a3f10e2f2c2859fce894
Sheng Cheng <charliecheng19882006@gmail.com>
2021-11-12 18:29:34 -0500

    first commit

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,33 @@
+package com.laioffer.tinnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.laioffer.tinnews.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        //NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
+    }
+}
